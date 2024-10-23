e332ef6987d99672eaa0b74e8c0285678a547171
Pedro-111 <108990464+Pedro-111@users.noreply.github.com>
2024-10-22 23:32:13 -0500

    Update hysteria_manager.sh

--- hysteria_manager.sh
+++ hysteria_manager.sh
@@ -271,7 +271,7 @@ change_passwords() {
     if [ ! -f "$CONFIG_FILE" ]; then
         echo -e "${RED}Hysteria no está instalado o configurado.${NC}"
         return
-    }
+    fi
 
     backup_config
 
